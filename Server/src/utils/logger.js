4c0b4107a584d94c460c08cd8071dfa49c8e7d66
nezahakim <142035259+nezahakim@users.noreply.github.com>
2024-10-24 18:01:47 +0200

    new

--- Server/src/utils/logger.js
+++ Server/src/utils/logger.js
@@ -11,7 +11,7 @@ const logger = winston.createLogger({
                 format: winston.format.simple(),
             }),
             new winston.transports.File({ filename: "error.log", level: "error"}),
-            new winston.transports.File({filename: 'combined.log' }),
+            new winston.transports.File({ filename: 'combined.log' }),
     ],
 });
 
